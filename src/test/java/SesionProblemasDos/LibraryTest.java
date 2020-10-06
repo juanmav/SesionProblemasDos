22fed87e4dd518908cd5a455161c520f04da1bc5
Juan Manuel Vicente <juanma.v82@gmail.com>
2020-10-06 12:28:48 +0200

    agrego nuevo test que va fallar.

--- src/test/java/SesionProblemasDos/LibraryTest.java
+++ src/test/java/SesionProblemasDos/LibraryTest.java
@@ -12,4 +12,10 @@ public class LibraryTest {
         Library classUnderTest = new Library();
         assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
     }
+    
+    @Test 
+    public void testSomeLibraryMethodTwo() {
+        Library classUnderTest = new Library();
+        assertFalse("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
 }
